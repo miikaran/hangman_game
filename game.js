428a5b5760e64220a1c162cda772379245b09359
miikaran <42628@edu.raseko.fi>
2022-10-15 22:51:45 +0300

    guess letters from random generated word

--- game.js
+++ game.js
@@ -2,15 +2,15 @@
 const ALPHABET =  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const WORDS = ['jade', 'piki', 'degu', 'tikru'];
 const LETTERS = document.getElementById('LETTERS');
-const WORD = document.getElementById('WORD')
+const WORD = document.getElementById('WORD');
 let WORD_TO_GUESS = '';
 
 INITIALIZE_GAME();
 
 
 function INITIALIZE_GAME(){
-    DISPLAY_LETTERS();
     GENERATE_WORD();
+    DISPLAY_LETTERS();
 }
 
 
@@ -19,6 +19,9 @@ function DISPLAY_LETTERS(){
         const BUTTON = document.createElement('BUTTON');
         BUTTON.textContent = LETTER;
         LETTERS.appendChild(BUTTON);
+        BUTTON.addEventListener('click', () => {
+            GUESS_LETTER(LETTER)
+        });
     })
 }
 
@@ -27,21 +30,34 @@ function GENERATE_WORD(){
     const RANDOM = Math.floor(Math.random() * WORDS.length);
     WORD_TO_GUESS = WORDS[RANDOM];
     const WORD_TO_LETTERS = WORD_TO_GUESS.split('');
-    //For each letter create new elements to display guess.
+    //For each letter create new elements to display them.
     WORD_TO_LETTERS.forEach((LETTER) => {
         const CHARACTER_CONTAINER = document.createElement('div');
         const CHARACTER = document.createElement('span');
         const UNDERLINE = document.createElement('div')
         CHARACTER.textContent = LETTER;
+        CHARACTER.setAttribute("id", LETTER)
         CHARACTER_CONTAINER.appendChild(CHARACTER);
         WORD.appendChild(CHARACTER_CONTAINER);
         CHARACTER_CONTAINER.appendChild(UNDERLINE)
+        //Set display to none at the start.
+        console.log(WORD_TO_GUESS)
+        CHARACTER.style.display = "none"
     })
 }
 
 
-function GUESS_LETTER(){
- //
+function GUESS_LETTER(LETTER){
+    ID = document.getElementById(LETTER)
+    if(WORD_TO_GUESS.includes(LETTER)){
+      console.log(ID.id)
+      ID.style.display = "block"
+    }
+}
+
+
+function CHECK_WIN(){
+
 }
 
 
