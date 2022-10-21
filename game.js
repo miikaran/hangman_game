3e4a794136a321dfe7e03ae8a53cdb1b1bbcdc17
miikaran <42628@edu.raseko.fi>
2022-10-22 00:19:47 +0300

    add hangman parts when guess wrong

--- game.js
+++ game.js
@@ -1,16 +1,34 @@
-
-const ALPHABET =  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-const WORDS = ['jade', 'piki', 'degu', 'tikru'];
+const ALPHABET =  [
+    'a', 'b', 'c', 'd', 'e', 
+    'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 
+    't', 'u', 'v', 'w', 'x', 'y', 'z'
+];
+const HANGMAN_PARTS_IDS = [
+    "FIRST", "SECOND", "THIRD", 
+    "FOURTH", "FIFTH", "SIXTH", 
+    "SEVENTH", "EIGHT", "NINTH", 
+    "TENTH", "ELEVENTH", "TWELTH", 
+    "THIRTEENTH"
+]
+const WORDS = [
+    'jade', 'piki', 
+    'degu', 'tikru'
+];
 const LETTERS = document.getElementById('LETTERS');
 const WORD = document.getElementById('WORD');
+const INFO = document.getElementById('INFO');
+const HINT_BUTTON = document.getElementById('HINT_BUTTON');
 let LETTER_ID = '';
 let WORD_TO_GUESS = '';
 let WINNING_ARRAY = [];
 let GUESSES = 0;
+let GAME_ON = false;
 
 INITIALIZE_GAME();
 
 function INITIALIZE_GAME(){
+    GAME_ON = true;
     GENERATE_WORD();
     DISPLAY_LETTERS();
 }
@@ -22,7 +40,7 @@ function DISPLAY_LETTERS(){
         BUTTON.textContent = LETTER;
         LETTERS.appendChild(BUTTON);
         BUTTON.addEventListener('click', () => {
-            GUESS_LETTER(LETTER)
+            GUESS_LETTER(LETTER);
         });
     })
 }
@@ -32,7 +50,7 @@ function GENERATE_WORD(){
     const RANDOM = Math.floor(Math.random() * WORDS.length);
     WORD_TO_GUESS = WORDS[RANDOM];
     const WORD_TO_LETTERS = WORD_TO_GUESS.split('');
-    //For each letter create new elements to display them.
+    //For each letter create new elements.
     WORD_TO_LETTERS.forEach((LETTER) => {
         const CHARACTER_CONTAINER = document.createElement('div');
         const CHARACTER = document.createElement('span');
@@ -46,35 +64,65 @@ function GENERATE_WORD(){
         console.log(WORD_TO_GUESS, CHARACTER)
         CHARACTER.style.display = "none"
     })
+    HINT_BUTTON.addEventListener('click', () => {
+        GET_HINT();
+    })
 }
 
 
 function GUESS_LETTER(LETTER){
-    LETTER_ID = document.querySelectorAll(`#${LETTER}`)
-    if(WORD_TO_GUESS.includes(LETTER)){
-        //Change display to block for all guessed letters.
-        for(let i = 0; i < LETTER_ID.length; i++) {
-            let CURRENT_LETTERS = LETTER_ID[i];
-            CURRENT_LETTERS.style.display = 'block';
-            CHECK_WIN(LETTER)
+    if(GAME_ON){
+        LETTER_ID = document.querySelectorAll(`#${LETTER}`)
+        if(WORD_TO_GUESS.includes(LETTER)){
+            //Change display to block for all guessed letters.
+            for(let i = 0; i < LETTER_ID.length; i++) {
+                let CURRENT_LETTERS = LETTER_ID[i];
+                CURRENT_LETTERS.style.display = 'block';
+                CHECK_WIN(LETTER)
+            }
         }
+        else{
+            GUESSES += 1;
+            //Display new hangman part.
+            const HANGMAN_PART = document.getElementById(HANGMAN_PARTS_IDS[GUESSES - 1]);
+            if(HANGMAN_PART == null){
+                GAME_ON = false;
+                INFO.style.color = 'red';
+                INFO.textContent = "You lost!"
+            }
+            else{
+                HANGMAN_PART.style.display = 'block';
+            }
+        }
+    }
+    else{
+        return null;
     }
 }
 
 
 function CHECK_WIN(LETTERS){
-    WINNING_ARRAY.push(LETTERS)
-    //Check if both arrays include same elements.
-    if(WINNING_ARRAY.length === WORD_TO_GUESS.split('').length){
-        return WINNING_ARRAY.every((element, index) => {
-            if(element == WORD_TO_GUESS[index] || WORD_TO_GUESS.includes(element)){
-                console.log('You won!')
-                return true;
-            }
-            else{
-                console.log('You no win')
-            }
-        })
+    if(GAME_ON){
+        WINNING_ARRAY.push(LETTERS)
+        //Check if both arrays include same elements.
+        if(WINNING_ARRAY.length === WORD_TO_GUESS.split('').length){
+            return WINNING_ARRAY.every((element, index) => {
+                GAME_ON = false;
+                if(element == WORD_TO_GUESS[index] || WORD_TO_GUESS.includes(element)){
+                    INFO.textContent = "You won!"
+                }
+                else{
+                    console.log('You no win')
+                }
+            })
+        }
+    }
+}
+
+
+function GET_HINT(){
+    if(GAME_ON == true){
+        
     }
 }
 
